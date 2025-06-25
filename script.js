3ba1cf9d8ee93884b273d4f610994ac2cebc54b4
Ek-Tor <hear8429@gmail.com>
2025-06-25 10:27:49 -0700

    Menu border delete.

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 
 // This is the circle going around//
+    // let circleX = 100;
+    // let circleY = 150;
+    // let circleRadius = 90;
+    // let angle = (0);
 
-let circleX = 100;
-let circleY = 150;
-let circleRadius = 90;
-let angle = (0);
+    // function setup() {
+    // createCanvas(200, 250);
+    // angleMode(DEGREES);
+    // describe();
+    // }
 
-function setup() {
-  createCanvas(200, 250);
-  angleMode(DEGREES);
-  describe();
-}
+    // function draw() {
+    // background(0);
+    // let angle = frameCount % 360;
+    // noFill();
+    // strokeWeight(20);
 
-function draw() {
-  background(0);
-  let angle = frameCount % 360;
-  noFill();
-  strokeWeight(20);
-
-  let pointX = circleX - circleRadius * sin(angle);
-  let pointY = circleY - circleRadius * cos(angle);
-  fill('');
-  circle(pointX, pointY, 30);
-}
+    // let pointX = circleX - circleRadius * sin(angle);
+    // let pointY = circleY - circleRadius * cos(angle);
+    // fill('');
+    // circle(pointX, pointY, 30);
+    // }
 
 //This is for the tabs//
 function openAnim(evt, animName) {
@@ -33,7 +32,7 @@ function openAnim(evt, animName) {
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(" active", "active");
   }
   document.getElementById(animName).style.display = "block";
   evt.currentTarget.className += " active";
