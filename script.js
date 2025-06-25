275d9a15cca9a876298cc79a6b06005e0f99cc9b
Ek-Tor <hear8429@gmail.com>
2025-06-25 09:42:25 -0700

    here is the adition of the tabs. adition of circle going around. tabs color and transparency. aditional comments.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let circleX = 200;
+
+// This is the circle going around//
+
+let circleX = 100;
 let circleY = 150;
-let circleRadius = 150;
+let circleRadius = 90;
 let angle = (0);
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 250);
   angleMode(DEGREES);
   describe();
 }
@@ -13,10 +16,28 @@ function draw() {
   background(0);
   let angle = frameCount % 360;
   noFill();
-  strokeWeight(3);
+  strokeWeight(20);
 
-  let pointX = circleX + circleRadius * cos(angle);
-  let pointY = circleY - circleRadius * sin(angle);
+  let pointX = circleX - circleRadius * sin(angle);
+  let pointY = circleY - circleRadius * cos(angle);
   fill('');
-  circle(pointX, pointY, 100);
+  circle(pointX, pointY, 30);
 }
+
+//This is for the tabs//
+function openAnim(evt, animName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(animName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
