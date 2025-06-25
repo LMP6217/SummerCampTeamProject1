44470ff36d47100fa33d8b8687d8164516e0df17
Ek-Tor <hear8429@gmail.com>
2025-06-25 15:23:33 -0700

    Aditions and adjustmnets, relocation of "creators" links to last tab. Waiting on updated code from animations. Working on style for title and text conten.

--- script.js
+++ script.js
@@ -7,13 +7,16 @@ function openAnim(evt, animName) {
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
+
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
+
   document.getElementById(animName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
 // Get the element with and click on it
 document.getElementById("defaultOpen").click();
+
