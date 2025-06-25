e06cbc39eda702879b3509e58ba1273b892a9fbc
Ek-Tor <hear8429@gmail.com>
2025-06-25 11:39:29 -0700

    leter tab shadows.

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
 
-// This is the circle going around//
-    // let circleX = 100;
-    // let circleY = 150;
-    // let circleRadius = 90;
-    // let angle = (0);
-
-    // function setup() {
-    // createCanvas(200, 250);
-    // angleMode(DEGREES);
-    // describe();
-    // }
-
-    // function draw() {
-    // background(0);
-    // let angle = frameCount % 360;
-    // noFill();
-    // strokeWeight(20);
-
-    // let pointX = circleX - circleRadius * sin(angle);
-    // let pointY = circleY - circleRadius * cos(angle);
-    // fill('');
-    // circle(pointX, pointY, 30);
-    // }
 
 //This is for the tabs//
 function openAnim(evt, animName) {
@@ -32,11 +9,11 @@ function openAnim(evt, animName) {
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "active");
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(animName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
-// Get the element with id="defaultOpen" and click on it
+// Get the element with and click on it
 document.getElementById("defaultOpen").click();
